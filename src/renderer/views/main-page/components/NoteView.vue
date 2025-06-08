e6f7614dbbea33537772b94af0af9be9668b1e19
huanxiao <490327763@qq.com>
2025-06-08 11:34:23 +0800

    feat: 初始化Editorjs hook

--- src/renderer/views/main-page/components/NoteView.vue
+++ src/renderer/views/main-page/components/NoteView.vue
@@ -1,13 +1,17 @@
 <template>
   <!-- <div ref="editorContainer" class="editor-container"></div> -->
   <div class="w-full h-full flex justify-center items-center flex-col relative">
-    <div class="max-w-3xl w-3xl h-full bg-blue-200"></div>
+    <div
+      class="max-w-3xl w-3xl h-full bg-blue-200"
+      ref="containerRef"
+      id="editorjs"
+    ></div>
   </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted, watch, onUnmounted } from 'vue';
 import { useNotes } from '../hooks/useNotes';
-import EditorJS from '@editorjs/editorjs';
+import { useEditorJs } from '../hooks/useEdjtorJs';
 import type { NoteContent } from '@common/models/note.types';
 
 const props = defineProps<{
@@ -16,28 +20,30 @@ const props = defineProps<{
 
 const { getNoteById } = useNotes();
 
-const note = ref<NoteContent | null>(null);
+const { editor, containerRef, initEditor, noteContent } = useEditorJs();
 
-const editor = ref<EditorJS | null>(null);
+const mountEditor = () => {};
 
 watch(
   () => props.noteId,
   async newVal => {
     if (newVal) {
       const res = await getNoteById(newVal);
+      console.log('res', res);
       if (res) {
-        note.value = res.content;
+        noteContent.value = res.content;
+        initEditor();
       }
     }
   }
 );
 
-onMounted(() => {
-  editor.value = new EditorJS({});
-});
+onMounted(() => {});
 
 onUnmounted(() => {
-  editor.value?.destroy();
+  if (editor) {
+    editor.destroy();
+  }
   // TODO: 保存笔记, 记录到最近打开队列中
 });
 </script>
--- src/renderer/views/main-page/hooks/useEdjtorJs.ts
+++ src/renderer/views/main-page/hooks/useEdjtorJs.ts
@@ -1,10 +1,73 @@
 import { ref } from 'vue';
 import EditorJS from '@editorjs/editorjs';
+import { NoteContent } from '@common/models/note.types';
+
+const editorBaseConfig = {};
+
+const editorJs = new EditorJS(editorBaseConfig);
 
 function useEditorJs() {
-  const editor = ref<EditorJS | null>(null);
+  const noteContent = ref<NoteContent | null>(null);
+  const containerRef = ref<HTMLDivElement | null>(null);
+  const readOnly = ref(false);
+  const saveLoading = ref(false);
 
-  return { editor };
+  let editor = null;
+
+  const initEditor = () => {
+    editor = new EditorJS({
+      ...editorBaseConfig,
+      holder: containerRef.value,
+      data: noteContent.value || { blocks: [] },
+      onReady: () => {
+        console.log('Editor.js is ready to work!');
+        renderEditor();
+      },
+      onChange: () => {
+        console.log('Editor.js content changed!');
+      },
+    });
+  };
+
+  const saveEditor = () => {
+    if (readOnly.value) {
+      return;
+    }
+    saveLoading.value = true;
+    editor
+      .save()
+      .then(outputData => {
+        noteContent.value = outputData;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        saveLoading.value = false;
+      });
+  };
+
+  const renderEditor = () => {
+    if (!noteContent.value) {
+      return;
+    }
+    if (typeof noteContent.value === 'string') {
+      editor.blocks.renderFromHTML(noteContent.value);
+    } else {
+      editor.blocks.render(noteContent.value);
+    }
+  };
+
+  return {
+    editor,
+    containerRef,
+    readOnly,
+    noteContent,
+    saveLoading,
+    initEditor,
+    saveEditor,
+    renderEditor,
+  };
 }
 
 export { useEditorJs };
