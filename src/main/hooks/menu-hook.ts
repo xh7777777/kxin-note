5e148a58573abcd67946be0c4d587e1de8894601
huanxiao <490327763@qq.com>
2025-06-09 23:12:20 +0800

    feat: 新菜单操作按钮

--- src/main/hooks/menu-hook.ts
+++ src/main/hooks/menu-hook.ts
@@ -21,6 +21,53 @@ const menu: Array<MenuItemConstructorOptions | MenuItem> = [
     ],
   },
   {
+    label: '编辑',
+    submenu: [
+      {
+        label: '撤销',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo',
+      },
+      {
+        label: '重做',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: '剪切',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut',
+      },
+      {
+        label: '复制',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy',
+      },
+      {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste',
+      },
+      {
+        label: '粘贴并匹配样式',
+        accelerator: 'CmdOrCtrl+Shift+V',
+        role: 'pasteAndMatchStyle',
+      },
+      {
+        label: '删除',
+        role: 'delete',
+      },
+      {
+        label: '全选',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectAll',
+      },
+    ],
+  },
+  {
     label: '帮助',
     submenu: [
       {
