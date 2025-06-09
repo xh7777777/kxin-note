a77a277557d3a856d09561312606e61624d67bbb
XIAOHUAN <490327763@qq.com>
2025-06-09 16:11:55 +0800

    feat: 安装基本插件

--- src/renderer/views/main-page/hooks/useEdjtorJs.ts
+++ src/renderer/views/main-page/hooks/useEdjtorJs.ts
@@ -1,11 +1,50 @@
 import { ref } from 'vue';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
+import Paragraph from '@editorjs/paragraph';
+import Quote from '@editorjs/quote';
+import Delimiter from '@editorjs/delimiter';
+import EditorjsList from '@editorjs/list';
 import { NoteContent, NotePage } from '@common/models/note.types';
 
 const editorBaseConfig = {
   tools: {
     header: Header,
+    paragraph: {
+      class: Paragraph,
+      inlineToolbar: true,
+      config: {
+        placeholder: '/输入快捷指令，alt+k唤起AI功能',
+      },
+    },
+    quote: {
+      class: Quote,
+      inlineToolbar: true,
+      shortcut: 'CMD+SHIFT+O',
+      config: {
+        quotePlaceholder: '输入快捷指令，alt+q唤起AI功能',
+        captionPlaceholder: '输入快捷指令，alt+c唤起AI功能',
+      },
+    },
+    delimiter: {
+      class: Delimiter,
+      inlineToolbar: true,
+      config: {
+        styleOptions: ['star', 'dash', 'line'],
+        defaultStyle: 'star',
+        lineWidthOptions: [8, 15, 25, 35, 50, 60, 100],
+        defaultLineWidth: 25,
+        lineThicknessOptions: [1, 2, 3, 4, 5, 6],
+        defaultLineThickness: 2,
+      },
+    },
+    list: {
+      class: EditorjsList,
+      inlineToolbar: true,
+      config: {
+        defaultStyle: 'unordered',
+      },
+    },
   },
 };
 
