adfd725813a30c4890763162cc8894cfc7723b7e
XIAOHUAN <490327763@qq.com>
2025-06-26 15:40:25 +0800

    feat: 修复tailwind样式导致的问题

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue';
 import router from './router';
 import { errorHandler } from './error';
 import './utils/hackIpcRenderer';
+import '@muyajs/core/lib/style.css';
 
 const app = createApp(App);
 const store = createPinia();
